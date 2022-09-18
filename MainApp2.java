887076f136163f122e5163d5ac623a820954816a
Putri Samyo Chania <113811177+PutriSamyoChania@users.noreply.github.com>
2022-09-18 13:38:19 +0700

    Add files via upload

---
+++ MainApp2.java
@@ -0,0 +1,9 @@
+public class MainApp2 {
+
+    public static void main(String[] args) {
+        PesawatTelpon b = new PesawatTelpon();
+
+        b.setTransmiter("Halloo");
+        System.out.println("Dengan Siapa " + b.getTransmiter());
+    }
+}
