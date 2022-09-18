3cfa9d4cb3da2ab239f0be37f2bc4d6d773a0498
Putri Samyo Chania <113811177+PutriSamyoChania@users.noreply.github.com>
2022-09-18 13:37:12 +0700

    Add files via upload

---
+++ MainApp1.java
@@ -0,0 +1,9 @@
+public class MainApp1 {
+
+    public static void main(String[] args) {
+        Kelinci p = new Kelinci();
+
+        p.setbulu(" lembut dan tebal");
+        System.out.println("Bulu ku sangat" + p.getBulu());
+    }
+}
