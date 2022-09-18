9d3ac66afee350529251560e573019768f6966ac
Putri Samyo Chania <113811177+PutriSamyoChania@users.noreply.github.com>
2022-09-18 13:36:02 +0700

    Add files via upload

---
+++ Kelinci.java
@@ -0,0 +1,52 @@
+public class Kelinci {
+    String mata, bulu, telinga, kaki, mulut;
+
+    public void setmata(String mata) {
+        this.mata = mata;
+    }
+
+    public void setbulu(String bulu) {
+        this.bulu = bulu;
+    }
+
+    public void setTelinga(String telinga) {
+        this.telinga = telinga;
+    }
+
+    public void setkaki(String kaki) {
+        this.kaki = kaki;
+    }
+
+    public void setmulut(String mulut) {
+        this.mulut = mulut;
+    }
+    
+
+    public String getMata() {
+        return mata;
+    }
+
+    public String getBulu() {
+        return bulu;
+    }
+
+    public String getTelinga() {
+        return telinga;
+    }
+
+    public String getKaki() {
+        return kaki;
+    }
+
+    public String getMulut() {
+        return mulut;
+    }
+
+    void bermain(String a) {
+        System.out.println("piiikkkk" + a);
+    }
+
+    void makan() {
+        System.out.println("wortellll");
+    }
+}
